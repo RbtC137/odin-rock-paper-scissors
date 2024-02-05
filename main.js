504b95d9cc435b526873477d3ddc334d7fa52e82
RbtC137 <128915939+RbtC137@users.noreply.github.com>
2024-02-05 23:26:19 +0800

    Change playRound function and add playGame function

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+const scoreBoard = [0, 0, 0]
 
 function getComputerChoice() {
     const rand = Math.random();
@@ -22,9 +19,28 @@ function playRound(playerSelection, computerSelection){
     }else if(playerSelection.toLowerCase() === 'paper'){
         res = computerSelection==='paper'?'Tie, both Paper'
         :computerSelection==='scissors'? 'You Lose! Scissors beats Paper': 'You Win! Paper beats Rock';
-    }else{
+    }else if(playerSelection.toLowerCase() === 'scissors'){
         res = computerSelection==='scissors'?'Tie, both Scissors'
         :computerSelection==='rock'? 'You Lose! Rock beats Scissors': 'You Win! Scissors beats Paper';
+    }else {
+        res = 'Invalid input!!!'
+    }
+    if(res.search('Win')!==-1){
+        scoreBoard[0] += 1;
+    }else if(res.search('Lose')!==-1){
+        scoreBoard[1] += 1;
+    }else {
+        scoreBoard[2] += 1;
     }
     return res
+}
+
+function playGame() {
+    let playerSelection = prompt('Show your move!');
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection),`player:${scoreBoard[0]}, computer:${scoreBoard[1]}, Tie: ${scoreBoard[2]}`)
+}
+
+while(1){
+    playGame();
 }
